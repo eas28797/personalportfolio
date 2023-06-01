6ad1653c8bf54c8c60412ddce66326074bbc7479
eas28797 <118745414+eas28797@users.noreply.github.com>
2023-06-01 10:17:45 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+let navLinks = document.querySelectorAll('nav a');
 
 window.onscroll = () => {
 	sections.forEach(sec => {
@@ -8,11 +8,10 @@ window.onscroll = () => {
 		let height = sec.offsetHeight;
 		let id = sec.getAttribute('id');
 		
-		if (top >= offset && top < offset + height) {
-			nabLinks.forEach(links => {
+		if (top > offset && top < offset + height) {
+			navLinks.forEach(links => {
 				links.classList.remove('active');
-				document.querySelector('header nav a [href*=' + id + ']').classList.add
-				('active');
+				document.querySelector('nav a [href*=' + id + ']').classList.add("active");
 			});
 			
 		};
@@ -20,3 +19,34 @@ window.onscroll = () => {
 	});
 	
 };
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
+
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+function show(){
+	mainMenu.style.display='flex';
+	mainMenu.style.top='0';
+	
+}
+
+function close(){
+	mainMenu.style.top = '-100%';
+}
